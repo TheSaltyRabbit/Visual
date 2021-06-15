3918b07a71f85b21664142f86a71f3bf3db6c6b5
Christian Mascunana <cmascunana@comcast.net>
2021-06-15 15:40:12 -0400

    Cleaned

--- Math.py
+++ Math.py
@@ -2,10 +2,8 @@
 import math
 
 class Shape:
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+    def __init__(self, sides):
+        self.sides = sides
 
 def CheckForFloat():
     while True:
@@ -16,55 +14,48 @@ def CheckForFloat():
             print ("Error: Entry was not a number")
     return x
 
+def GetSides(x,name1, name2, name3):
+    name = [name1,name2,name3]
+    for i in range (0,x):
+        print ("\nPlease input %s"%(name[i]))
+        shape.sides[i]=CheckForFloat()
+
 def AreaMath():
     while True:
         print ("\nArea")
         print ("Enter 'S' for Square, 'R' for Rectangle, 'T' for Triangle,\n'RT' for Right Triangle or 'C' for Circle")
         print ("Enter B to return to the Main Menu")
         x = input("User=> ").upper()
-        if (x=='S'): #for squares a is the side length
-            print ("\nPlease input the side length")
-            shape.a = CheckForFloat()
-            area = shape.a**2
+        if (x=='S'):
+            GetSides(1,"The Length of the Side","","")
+            shape.sides[3] = shape.sides[0]**2
             shapename = "Square"
             break
-        elif (x=='R'): #for rectangles a is length b is width
-            print ("\nPlease input the length")
-            shape.a = CheckForFloat()
-            print ("\nPlease input the width")
-            shape.b = CheckForFloat()
-            area = shape.a*shape.b
+        elif (x=='R'):
+            GetSides(2,"the Length","the Width","")
+            shape.sides[3] = shape.sides[0]*shape.sides[1]
             shapename = "Rectangle"
             break
-        elif (x=='RT'): #for right triangle a is base and b is height
-            print ("\nPlease input the base")
-            shape.a = CheckForFloat()
-            print ("\nPlease input the height")
-            shape.b = CheckForFloat()
-            area = 0.5*shape.a*shape.b
+        elif (x=='RT'):
+            GetSides(2,"the Base","the Height","")
+            shape.sides[3] = 0.5*shape.sides[0]*shape.sides[1]
             shapename = "Right Triangle"
             break
         elif (x=='T'):
-            print ("\nPlease input side A length")
-            shape.a=CheckForFloat()
-            print ("\nPlease input side B length")
-            shape.b=CheckForFloat()
-            print ("\nPlease input side C length")
-            shape.c=CheckForFloat()
-            s = (shape.a+shape.b+shape.c)/2
-            area = math.sqrt(s*(s-shape.a)*(s-shape.b)*(s-shape.c))
+            GetSides(3,"Side A","Side B","Side C")
+            s = (shape.sides[0]+shape.sides[1]+shape.sides[2])/2
+            shape.sides[3] = math.sqrt(s*(s-shape.sides[0])*(s-shape.sides[1])*(s-shape.sides[2]))
             shapename = "Triangle"
             break
         elif (x=='C'):
-            print ("\nPlease input the radius")
-            shape.a=CheckForFloat()
-            area = math.pi*shape.a**2
+            GetSides(1,"the Radius","","")
+            shape.sides[3] = math.pi*shape.sides[0]**2
             shapename = "Circle"
             break
         else:
             print ("Error: Invalid Entry")
 
-    print ("The Area of the %s is: %s\n" %(shapename,area))
+    print ("The Area of the %s is: %s\n" %(shapename,shape.sides[3]))  
 
 def PerimeterMath():
     while True:
@@ -72,48 +63,35 @@ def PerimeterMath():
         print ("Enter 'S' for Square, 'R' for Rectangle, 'T' for Triangle,\n'RT' for Right Triangle or 'C' for Circle")
         print ("Enter B to return to the Main Menu")
         x = input("User=> ").upper()
-        if (x=='S'): #for squares a is the side length
-            print ("\nPlease input the side length")
-            shape.a = CheckForFloat()
-            perimeter = shape.a*4
+        if (x=='S'):
+            GetSides(1,"The Length of the Side","","")
+            shape.sides[3] = shape.sides[0]*4
             shapename = "Square"
             break
-        elif (x=='R'): #for rectangles a is length b is width
-            print ("\nPlease input the length")
-            shape.a = CheckForFloat()
-            print ("\nPlease input the width")
-            shape.b = CheckForFloat()
-            perimeter = 2*shape.a + 2*shape.b
+        elif (x=='R'):
+            GetSides(2,"the Length","the Width","")
+            shape.sides[3] = 2*shape.sides[0] + 2*shape.sides[1]
             shapename = "Rectangle"
             break
-        elif (x=='RT'): #for right triangle a is base and b is height
-            print ("\nPlease input the base")
-            shape.a = CheckForFloat()
-            print ("\nPlease input the height")
-            shape.b = CheckForFloat()
-            perimeter = shape.a + shape.b + math.sqrt(shape.a**2 + shape.b**2)
+        elif (x=='RT'):
+            GetSides(2, "the Base", "the Height","")
+            shape.sides[3] = shape.sides[0] + shape.sides[1] + math.sqrt(shape.sides[0]**2 + shape.sides[1]**2)
             shapename = "Right Triangle"
             break
         elif (x=='T'):
-            print ("\nPlease input side A length")
-            shape.a=CheckForFloat()
-            print ("\nPlease input side B length")
-            shape.b=CheckForFloat()
-            print ("\nPlease input side C length")
-            shape.c=CheckForFloat()
-            perimeter = shape.a + shape.b + shape.c
+            GetSides(3, "Side A", "Side B", "Side C")
+            shape.sides[3] = shape.sides[0] + shape.sides[1] + shape.sides[2]
             shapename = "Triangle"
             break
         elif (x=='C'):
-            print ("\nPlease input the radius")
-            shape.a=CheckForFloat()
-            perimeter = 2 * math.pi * shape.a
+            GetSides(1, "the Radius","","")
+            shape.sides[3] = 2 * math.pi * shape.sides[0]
             shapename = "Circle"
             break
         else:
             print ("Error: Invalid Entry")
 
-    print ("The Perimeter of the %s is: %s\n" %(shapename,perimeter))
+    print ("The Perimeter of the %s is: %s\n" %(shapename,shape.sides[3]))  
 
 def VolumeMath():
     while True:
@@ -121,61 +99,45 @@ def VolumeMath():
         print ("Enter 'C' for Cube, 'RRP' for Right Rectangular Prism, 'PR' for Prism , 'CY' for Cylinder,\n'PY' for Pyramid, 'CO' for Cone or 'S' for Sphere")
         print ("Enter B to return to the Main Menu")
         x = input("User=> ").upper()
-        if (x=='C'): #for squares a is the side length
-            print ("\nPlease input the side length")
-            shape.a = CheckForFloat()
-            volume = shape.a**3
+        if (x=='C'):
+            GetSides(1, "the Length of the Side","","")
+            shape.sides[3] = shape.sides[0]**3
             shapename = "Cube"
             break
-        elif (x=='RRP'): #for rectangles a is length b is width
-            print ("\nPlease input the length")
-            shape.a = CheckForFloat()
-            print ("\nPlease input the width")
-            shape.b = CheckForFloat()
-            print ("\nPlease input the height")
-            volume = shape.a*shape.b*shape.c
+        elif (x=='RRP'):
+            GetSides(3,"the Length","the Width","the Height")
+            shape.sides[3] = shape.sides[0]*shape.sides[1]*shape.sides[2]
             shapename = "Rectangle"
             break
-        elif (x=='PR') or (x=='CY'): #for right triangle a is base and b is height
-            print ("\nPlease input the area of the base")
-            shape.a = CheckForFloat()
-            print ("\nPlease input the height")
-            shape.b = CheckForFloat()
-            volume = shape.a*shape.b
-            if (x=='PR'): shapename = "Prism"
-            else: shapename = "Cylinder"
-            break
-        elif (x=='PY') or (x=='CO'):
-            print ("\nPlease input the area of the base")
-            shape.a = CheckForFloat()
-            print ("\nPlease input the height")
-            shape.b = CheckForFloat()
-            volume = (1/3) * shape.a * shape.b
-            if (x=='PY'): shapename = "Pyramid"
-            else: shapename = "Cone"
+        elif (x=='PR') or (x=='CY') or (x=='PY') or (x=='CO'):
+            GetSides(2,"the Area of the Base","the Height","")
+            if (x=='PR') or (x=='CY'):
+                shape.sides[3] = shape.sides[0]*shape.sides[1]
+                if (x=='PR'): shapename = "Prism"
+                else: shapename = "Cylinder"
+            else:
+                shape.sides[3] = (1/3) * shape.sides[0] * shape.sides[1]
+                if (x=='PY'): shapename = "Pyramid"
+                else: shapename = "Cone" 
             break
         elif (x=='S'):
-            print ("\nPlease input the radius")
-            shape.a=CheckForFloat()
-            volume = (4/3) * math.pi * (shape.a**3)
+            GetSides(1, "the Radius","","")
+            shape.sides[3] = (4/3) * math.pi * (shape.sides[0]**3)
             shapename = "Sphere"
             break
         else:
             print ("Error: Invalid Entry")
 
-    print ("The Area of the %s is: %s\n" %(shapename,volume))
+    print ("The Volume of the %s is: %s\n" %(shapename,shape.sides[3]))
 
 def ResetShape():
-    shape.a=0
-    shape.b=0
-    shape.c=0
-
+    shape.sides = [0,0,0,0]
 
 global shape
-shape = Shape(0,0,0)
+shape = Shape([0,0,0,0])
 while True:
     print ("Main Menu")
-    print ("Enter 'A' for Area, 'P' for Perimeter or 'V' for volume.")
+    print ("Enter 'A' for Area, 'P' for Perimeter or 'V' for shape.sides[3].")
     print ("Enter 'Q' to quit")
     x = input("User=> ").upper()
 
